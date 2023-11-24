f3b081e9eaa82deb6c05d381d25919707e070121
Nghia La <leonghiacnn@gmail.com>
2023-11-24 22:56:00 +0700

    Installed TailwindCSS and some plugins

---
+++ Examonimy/ExamonimyWeb/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./wwwroot/**/*.{html,js}", "./Views/**/*.cshtml"],
+    theme: {
+        extend: {},
+    },
+    corePlugins: {
+        aspectRatio: false,
+    },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+    ],
+}
+
