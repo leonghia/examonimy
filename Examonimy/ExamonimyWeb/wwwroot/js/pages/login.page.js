378b33b6d611124be7c071a66bab1f6a7ec3bddc
Nghia La <leonghiacnn@gmail.com>
2023-11-25 00:15:39 +0700

    Create UI for login and register pages

---
+++ Examonimy/ExamonimyWeb/wwwroot/js/pages/login.page.js
@@ -0,0 +1,13 @@
+﻿// DOM selectors
+const htmlElement = document.documentElement;
+// States and rules
+
+// Function expressions
+
+// Event listeners
+
+// Onload
+(() => {
+    htmlElement.classList.remove("bg-white");
+    htmlElement.classList.add("bg-gray-50");
+})();
--- Examonimy/ExamonimyWeb/wwwroot/js/site.js
+++ Examonimy/ExamonimyWeb/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// DOM selectors
+
+// States and rules
+
+// Function expressions
+
+// Event listeners
+
+// On load
