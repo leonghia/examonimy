c84f76e7edbc24871eff7aacb0d0cd03d36baa39
Nghia La <leonghiacnn@gmail.com>
2024-01-03 19:55:44 +0700

    Exam paper quickview

--- Examonimy/ExamonimyWeb/wwwroot/js/pages/exam-create.page.js
+++ Examonimy/ExamonimyWeb/wwwroot/js/pages/exam-create.page.js
@@ -3,6 +3,7 @@ import { fetchData } from "../helpers/ajax.helper.js";
 import { MediaType } from "../helpers/media-type.helper.js";
 import { ExamPaper } from "../models/exam-paper.model.js";
 import { ExamPaperRequestParams } from "../models/request-params.model.js";
+import { ExamCreate } from "../models/exam.model.js";
 
 // DOM selectors
 const courseListContainer = document.querySelector("#course-list-container");
@@ -12,7 +13,7 @@ const emptyExamPaper = document.querySelector("#empty-exam-paper");
 
 
 // States
-
+const examCreate = new ExamCreate();
 
 // Functions
 const fetchExamPapersByCourseId = async (courseId = 0) => {
@@ -40,7 +41,8 @@ const populateExamPapers = (examPapers = [new ExamPaper()]) => {
         <label for="exam-paper-${ep.id}" class="font-medium text-gray-900">${ep.examPaperCode}</label>
         <p class="text-gray-500">Tác giả: ${ep.authorName}</p>
     </div>
-    <div class="ml-3 flex h-6 items-center">
+    <div class="flex h-6 items-center gap-x-6">
+        <a href="/exam-paper/${ep.id}" target="_blank" class="bg-indigo-200 px-2 py-1 rounded-md text-xs font-semibold text-indigo-700 hover:text-indigo-800 hover:bg-indigo-300">Xem chi tiết</a>       
         <input id="exam-paper-${ep.id}" name="exam-paper" value="${ep.id}" type="radio" class="h-4 w-4 border-none text-violet-600 bg-gray-300 focus:ring-0 focus:ring-offset-0">
     </div>
 </div>
@@ -53,7 +55,17 @@ courseListContainer.addEventListener("click", event => {
     if (clickedRadio) {
         selectedCourseElement.textContent = clickedRadio.parentElement.parentElement.querySelector("label").textContent;
         fetchExamPapersByCourseId(Number(clickedRadio.value));
+        return;
     }
 });
+
+examPaperListContainer.addEventListener("click", event => {
+    const clickedRadio = event.target.closest("input");
+    if (clickedRadio) {
+        examCreate.examPaperId = Number(clickedRadio.value);
+        return;
+    }
+});
+
 
 // On load
